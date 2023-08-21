d87d2ace424f02a125fb99549293f7d57b718126
Felipe Velêz <60145791+devel-ez@users.noreply.github.com>
2023-08-21 17:25:11 -0300

    Updo BD

--- gestsalcnew.sql
+++ gestsalcnew.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 21-Ago-2023 às 17:28
+-- Tempo de geração: 21-Ago-2023 às 22:24
 -- Versão do servidor: 10.4.28-MariaDB
 -- versão do PHP: 8.0.28
 
@@ -60,15 +60,13 @@ CREATE TABLE `historico_data_entrada_saida` (
 --
 
 INSERT INTO `historico_data_entrada_saida` (`id_hist_entrada_saida`, `id_processos`, `hist_data_entrada`, `hist_motivo_data_entrada`, `hist_data_saida`, `hist_motivo_data_saida`) VALUES
-(1, 3, '2023-05-10', 'tEste 3', '2023-08-08', 'tEste 3'),
-(2, 4, '2023-08-08', 'motivo da entrata teste 4', '2023-08-08', 'motivo da entrata teste 4'),
-(3, 3, '2014-04-03', 'tEste 3 updat entrada', '2023-08-01', 'tEste 3 update saida'),
-(4, 5, '2023-08-15', '', '0000-00-00', ''),
-(5, 6, '2023-08-02', '', '0000-00-00', ''),
-(6, 7, '2023-08-08', '', '0000-00-00', ''),
-(7, 8, '2023-08-03', '', '0000-00-00', ''),
-(8, 9, '2023-08-31', '', '0000-00-00', ''),
-(9, 10, '2023-08-09', '', '0000-00-00', '');
+(24, 23, '2023-08-02', '', '0000-00-00', ''),
+(25, 24, '2023-08-15', '', '0000-00-00', ''),
+(26, 23, '2023-08-11', '', '0000-00-00', ''),
+(27, 24, '2023-08-11', '', '0000-00-00', ''),
+(28, 25, '2023-08-03', '', '0000-00-00', ''),
+(29, 23, '2023-08-09', '', '0000-00-00', ''),
+(30, 24, '2023-08-09', '', '0000-00-00', '');
 
 -- --------------------------------------------------------
 
@@ -103,16 +101,9 @@ CREATE TABLE `processos` (
 --
 
 INSERT INTO `processos` (`id_processos`, `data_entrada`, `motivo_data_entrada`, `data_saída`, `motivo_data_saida`, `dfd`, `NUP`, `tipo_processo_origem`, `numero_processo_origem`, `assunto_objeto`, `descricao_detalhada_objeto`, `requisitante`, `analista`, `previsao_conclusao`, `situacao`, `operador_fase_externa`, `resultado_fase_externa`, `protocolista`, `status`) VALUES
-(1, '2023-08-02', 'teste motivo da primeira entrada', '2023-08-08', 'teste motivo da primeira entrada', '01/2023', '0', 'Pregão SRP', '01/2023', 'Teste do objeto', '', 'citex', 'roque', '2023-08-31', 'fase 2', 'velez', 'sessão pública', 'emanoel', 'conformidade'),
-(2, '2023-08-02', 'teste motivo da primeira entrada', '2023-08-08', 'teste motivo da primeira saida', '01/2023', '0', 'pregao srp', '01/2023', 'teste objeto', '', 'tete requisitante', 'teste analista', '2023-08-31', 'teste situação', 'teste operador', 'teste resultado da fase externa', 'teste protocolista', 'teste status do protocolo'),
-(3, '2014-04-03', 'tEste 3 updat entrada', '2023-08-01', 'tEste 3 update saida', 'tEste 3', '0', 'tEste 3', 'tEste 3', '', '', 'tEste 3', 'tEste 3', '2023-08-05', 'tEste 3', 'tEste 3', 'tEste 3', 'tEste 3', 'tEste 3'),
-(4, '2023-08-08', 'motivo da entrata teste 4', '2023-08-08', 'motivo da entrata teste 4', '', '0', '', '', '', '', '', '', '0000-00-00', '', '', '', '', ''),
-(5, '2023-08-15', '', '0000-00-00', '', '', '11111', 'Pregão SRP', '11111/1111', '111', '111', '7ºCT', '', '0000-00-00', 'Na fila', '', '', '', ''),
-(6, '2023-08-02', '', '0000-00-00', '', '', '22222', 'Pregão SRP', '22222/2222', '222', '222', '7ºCT', '', '0000-00-00', 'Na fila', '', '', '', ''),
-(7, '2023-08-08', '', '0000-00-00', '', '', '11111', 'Aguardando definição', '00000/0000', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'PAC', '', '0000-00-00', 'Fase 5 - Saneamento', '', '', '', ''),
-(8, '2023-08-03', '', '0000-00-00', '', '', '22222', 'Inexigibilidade', '22222/2222', '222', '22', '7ºCT', '', '0000-00-00', 'Na fila', '', '', '', ''),
-(9, '2023-08-31', '', '0000-00-00', '', '', '33333', 'Pregão SRP', '33333/3333', '333333333333333333333333333333', '333333333333', '7ºCT', '', '0000-00-00', 'Na fila', '', '', '', ''),
-(10, '2023-08-09', '', '0000-00-00', '', '', '44444.444444/4444-44', 'Pregão IRP', '44444/4444', '44', '444', 'DCI', '', '0000-00-00', 'Fase 3 - Adequação EPC', '', '', '', '');
+(23, '2023-08-09', '', '0000-00-00', '', '', '11111', 'Pregão IRP', '11111/1111', '11111', 'dfgdsgfgsd', 'AC Defesa', '', '0000-00-00', 'Fase 2 - Análise SALC', '', '', '', ''),
+(24, '2023-08-09', '', '0000-00-00', '', '', '11111', 'Pregão IRP', '11111/1111', '11111', 'dfgdsgfgsd', 'AC Defesa', '', '0000-00-00', 'Fase 2 - Análise SALC', '', '', '', ''),
+(25, '2023-08-03', '', '0000-00-00', '', '', '22222.222222/2222-22', 'Dispensa Eletrônica Sem Disputa', '22222/2222', '222', '222', 'PAC', '', '0000-00-00', 'Fase 5 - Saneamento', '', '', '', '');
 
 --
 -- Acionadores `processos`
@@ -157,13 +148,13 @@ ALTER TABLE `processos`
 -- AUTO_INCREMENT de tabela `historico_data_entrada_saida`
 --
 ALTER TABLE `historico_data_entrada_saida`
-  MODIFY `id_hist_entrada_saida` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_hist_entrada_saida` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT de tabela `processos`
 --
 ALTER TABLE `processos`
-  MODIFY `id_processos` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_processos` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- Restrições para despejos de tabelas
