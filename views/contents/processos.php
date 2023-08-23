f96d57b8f27b56220bd609e168efc3c7fe3132c3
Felipe Velêz <60145791+devel-ez@users.noreply.github.com>
2023-08-23 12:26:38 -0300

    Editando modal kanban

--- views/contents/processos.php
+++ views/contents/processos.php
@@ -220,6 +220,198 @@
     </div>
 </div>
 <!-- /Tela modal para cadastro de processo -->
+
+<!-- Tela modal para kanban do processo -->
+<div class="modal fade modal-xl-custom" id="mdlKanbanProcesso" role="dialog">
+
+    <div class="modal-dialog modal-xl-custom">
+        <!-- Conteúdo do modal -->
+        <div class="modal-content">
+
+            <!-- Cabecalho do modal -->
+            <div class="modal-header bg-success py-1">
+                <h5 class="modal-title">Tarefas do processo</h5>
+                <button type="button" class="btn btn-outline-primary text-white border-0 fs-5" id="btnFecharModal" data-dismiss="modal">
+                    <i class="far fa-times-circle"></i>
+                </button>
+            </div>
+            <!-- Corpo do modal -->
+            <div class="modal-body">
+                <div class="content-wrapper kanban">
+                    <section class="content pb-3">
+                        <div class="container-fluid h-100">
+                            <div class="card card-row card-secondary">
+                                <div class="card-header">
+                                    <h3 class="card-title">
+                                        Backlog
+                                    </h3>
+                                </div>
+                                <div class="card-body">
+                                    <div class="card card-info card-outline">
+                                        <div class="card-header">
+                                            <h5 class="card-title">Create Labels</h5>
+                                            <div class="card-tools">
+                                                <a href="#" class="btn btn-tool btn-link">#3</a>
+                                                <a href="#" class="btn btn-tool">
+                                                    <i class="fas fa-pen"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="card-body">
+                                            <div class="custom-control custom-checkbox">
+                                                <input class="custom-control-input" type="checkbox" id="customCheckbox1" disabled>
+                                                <label for="customCheckbox1" class="custom-control-label">Bug</label>
+                                            </div>
+                                            <div class="custom-control custom-checkbox">
+                                                <input class="custom-control-input" type="checkbox" id="customCheckbox2" disabled>
+                                                <label for="customCheckbox2" class="custom-control-label">Feature</label>
+                                            </div>
+                                            <div class="custom-control custom-checkbox">
+                                                <input class="custom-control-input" type="checkbox" id="customCheckbox3" disabled>
+                                                <label for="customCheckbox3" class="custom-control-label">Enhancement</label>
+                                            </div>
+                                            <div class="custom-control custom-checkbox">
+                                                <input class="custom-control-input" type="checkbox" id="customCheckbox4" disabled>
+                                                <label for="customCheckbox4" class="custom-control-label">Documentation</label>
+                                            </div>
+                                            <div class="custom-control custom-checkbox">
+                                                <input class="custom-control-input" type="checkbox" id="customCheckbox5" disabled>
+                                                <label for="customCheckbox5" class="custom-control-label">Examples</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card card-primary card-outline">
+                                        <div class="card-header">
+                                            <h5 class="card-title">Create Issue template</h5>
+                                            <div class="card-tools">
+                                                <a href="#" class="btn btn-tool btn-link">#4</a>
+                                                <a href="#" class="btn btn-tool">
+                                                    <i class="fas fa-pen"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="card-body">
+                                            <div class="custom-control custom-checkbox">
+                                                <input class="custom-control-input" type="checkbox" id="customCheckbox1_1" disabled>
+                                                <label for="customCheckbox1_1" class="custom-control-label">Bug Report</label>
+                                            </div>
+                                            <div class="custom-control custom-checkbox">
+                                                <input class="custom-control-input" type="checkbox" id="customCheckbox1_2" disabled>
+                                                <label for="customCheckbox1_2" class="custom-control-label">Feature Request</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card card-primary card-outline">
+                                        <div class="card-header">
+                                            <h5 class="card-title">Create PR template</h5>
+                                            <div class="card-tools">
+                                                <a href="#" class="btn btn-tool btn-link">#6</a>
+                                                <a href="#" class="btn btn-tool">
+                                                    <i class="fas fa-pen"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card card-light card-outline">
+                                        <div class="card-header">
+                                            <h5 class="card-title">Create Actions</h5>
+                                            <div class="card-tools">
+                                                <a href="#" class="btn btn-tool btn-link">#7</a>
+                                                <a href="#" class="btn btn-tool">
+                                                    <i class="fas fa-pen"></i>
+                                                </a>
+                                            </div>
+
+                                        </div>
+                                        <div class="card-body">
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                                                Aenean commodo ligula eget dolor. Aenean massa.
+                                                Cum sociis natoque penatibus et magnis dis parturient montes,
+                                                nascetur ridiculus mus.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card card-row card-primary">
+                                <div class="card-header">
+                                    <h3 class="card-title">
+                                        To Do
+                                    </h3>
+                                </div>
+                                <div class="card-body">
+                                    <div class="card card-primary card-outline">
+                                        <div class="card-header">
+                                            <h5 class="card-title">Create first milestone</h5>
+                                            <div class="card-tools">
+                                                <a href="#" class="btn btn-tool btn-link">#5</a>
+                                                <a href="#" class="btn btn-tool">
+                                                    <i class="fas fa-pen"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card card-row card-default">
+                                <div class="card-header bg-info">
+                                    <h3 class="card-title">
+                                        In Progress
+                                    </h3>
+                                </div>
+                                <div class="card-body">
+                                    <div class="card card-light card-outline">
+                                        <div class="card-header">
+                                            <h5 class="card-title">Update Readme</h5>
+                                            <div class="card-tools">
+                                                <a href="#" class="btn btn-tool btn-link">#2</a>
+                                                <a href="#" class="btn btn-tool">
+                                                    <i class="fas fa-pen"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="card-body">
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                                                Aenean commodo ligula eget dolor. Aenean massa.
+                                                Cum sociis natoque penatibus et magnis dis parturient montes,
+                                                nascetur ridiculus mus.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card card-row card-success">
+                                <div class="card-header">
+                                    <h3 class="card-title">
+                                        Done
+                                    </h3>
+                                </div>
+                                <div class="card-body">
+                                    <div class="card card-primary card-outline">
+                                        <div class="card-header">
+                                            <h5 class="card-title">Create repo</h5>
+                                            <div class="card-tools">
+                                                <a href="#" class="btn btn-tool btn-link">#1</a>
+                                                <a href="#" class="btn btn-tool">
+                                                    <i class="fas fa-pen"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+
+            </div>
+            <!-- Corpo do modal -->
+        </div>
+    </div>
+</div>
+<!-- /Tela modal para kanban do processo -->
 <!-- Ajax scripts -->
 <script>
     var Toast = swal.mixin({
@@ -329,7 +521,7 @@
                     orderable: false,
                     render: function(data, type, full, meta) {
                         return "<center>" +
-                            "<span class='btnPainelProcesso text-navy px-1' style='cursor: pointer;'>" +
+                            "<span class='btnKanbanProcesso text-navy px-1' style='cursor: pointer;'>" +
                             "<i class='fas fa-clipboard fs-5'></i>" +
                             "</span>" +
 
@@ -669,5 +861,28 @@
             }
         })
 
+    });
+
+    /* -------------------------------------------------------------------------- */
+    /*                     Abre o kanban do processo no modal                     */
+    /* -------------------------------------------------------------------------- */
+
+    $("#example1").on("click", ".btnKanbanProcesso", function() {
+
+        action = 5;
+
+        $("#mdlKanbanProcesso").modal('show');
+
+        // var data = table.row($(this).parents('tr')).data();
+
+        // $("#idId").val(data[0]);
+        // $("#idNup").val(data[5]);
+        // $("#selProcesso").val(data[3]);
+        // $("#idNrProcesso").val(data[4]);
+        // $("#selRequisitante").val(data[10]);
+        // $("#selFase").val(data[11]);
+        // $("#idDescricaoResumida").val(data[7]);
+        // $("#idDescricaoDetalhada").val(data[8]);
+        // $("#idDataEntrada").val(data[9]);
     });
 </script>
