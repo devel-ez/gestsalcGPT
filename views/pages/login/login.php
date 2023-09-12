88057cd7f987fcdd40730fbfa827f1cbb1c580e7
devel-ez <felipevelez.adm@hotmail.com>
2023-09-11 22:05:57 -0300

    Background card login

--- views/pages/login/login.php
+++ views/pages/login/login.php
@@ -6,26 +6,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>AdminLTE 3 | Log in</title> -->
 
-    <!-- Google Font: Source Sans Pro -->
-    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"> -->
-    <!-- Font Awesome -->
-    <!-- <link rel="stylesheet" href="../../assets/plugins/fontawesome-free/css/all.min.css"> -->
-    <!-- icheck bootstrap -->
-    <!-- <link rel="stylesheet" href="../../assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css"> -->
-    <!-- Theme style -->
-    <!-- <link rel="stylesheet" href="../../assets/dist/css/adminlte.min.css"> -->
-    
+<!-- Google Font: Source Sans Pro -->
+<!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback"> -->
+<!-- Font Awesome -->
+<!-- <link rel="stylesheet" href="../../assets/plugins/fontawesome-free/css/all.min.css"> -->
+<!-- icheck bootstrap -->
+<!-- <link rel="stylesheet" href="../../assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css"> -->
+<!-- Theme style -->
+<!-- <link rel="stylesheet" href="../../assets/dist/css/adminlte.min.css"> -->
+
 <!-- </head> -->
 
 <body class="hold-transition login-page">
     <div class="login-box">
+        <div class="login-logo">
+            <a><b>Gest</b>SALC</a>
+        </div>
 
         <!-- /.login-logo -->
         <div class="card">
-            <div class="card card-outline card-navy">
-                <div class="login-logo">
-                    <a><b>Gest</b>SALC</a>
-                </div>
+
+            <div class="card card-outline card-navy cardBackground">
+
                 <div class="card-body">
                     <!-- Formulário de login -->
                     <form method="post" class="needs-validation  needs-validation-login" novalidate>
